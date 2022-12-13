178774933589d3058e4a1e4905c31e03392f3bc5
Ramadhana R <ddhanarr@gmail.com>
2022-12-13 18:23:22 +0700

    feat: dataset 1 frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import * as tf from '@tensorflow/tfjs';
+import DiagnosisForm from "../components/diagnosis-form";
 
-async function loadModel() {
-  const model = await tf.loadLayersModel('./model/model.json');
-  return 'haha'
-}
-
+// async function getDiagnosis() {
+//   const coba = await fetch("http://127.0.0.1:8000/path");
+//   return await coba.json();
+// }
 export default async function Home() {
-  const coba = await loadModel()
   return (
-    <div>{coba}</div>
-  )
+    <main className="w-screen h-screen bg-indigo-100 flex sm:px-0 px-3 justify-center flex-col items-center">
+      <h1 className="text-2xl font-bold mb-6">
+        Final Project Analitika Bisnis
+      </h1>
+      <DiagnosisForm />
+    </main>
+  );
 }
